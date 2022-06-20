8ebbafe026cfc3826d8f84711efbd7dc9e414453
Bruno Nogueira <brunovnogueira@gmail.com>
2022-06-20 12:29:46 -0300

    corrigindo exercicio7

--- Modulo 1/Lista1/src/Exercicio7.java
+++ Modulo 1/Lista1/src/Exercicio7.java
@@ -2,8 +2,8 @@ public class Exercicio7 {
     public static void main(String[] args) {
         int A = 10;
         int B = 20;
-        A += 10;
-        B -= 10;
+        B -= A;
+        A += B;
 
         System.out.println("Valor de A = " + A);
         System.out.println("Valor de B = " + B);
